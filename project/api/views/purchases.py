07d235eb2ee539eea08969e7832e7867f99d4ce1
timehollyname <time.holly.name@gmail.com>
2021-06-11 11:50:13 +0300

    commit

--- project/api/views/purchases.py
+++ project/api/views/purchases.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from rest_framework.viewsets import ViewSet
@@ -9,6 +10,7 @@ from ..serializers import PurchasesSerializer
 
 class PurchasesViewSet(ViewSet):
     serializer_class = PurchasesSerializer
+    permission_classes = AllowAny
     http_method_names = ('get', 'post', 'delete',)
     purchases = None
 
